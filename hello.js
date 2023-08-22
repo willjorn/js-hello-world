764b5bd865d3d3a9affb086fb9550bdecb207f0d
willjorn <klpumagoi@gmail.com>
2023-08-21 22:15:47 -0500

    Hello World

--- hello.js
+++ hello.js
@@ -4,19 +4,57 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Hello, World!'); // Task 1: Print 'Hello, World!'
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+*/
+console.log('Hello, Universe!'); // Task 1 solution: Change the message
+
+/*  
    2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+*/
+// console.log 'Hello, World!'; // Code won't work without parentheses
+// The parentheses are used to enclose the argument being passed to the console.log function.
+
+/*  
+   3. Remove one or both quotation marks.
+*/
+// console.log(Hello, World!); // Code won't work without quotes around the string
+// console.log('Hello, World!); // Code won't work without closing quote
+
+/*  
    4. Remove the semi-colon, ;.
+*/
+// console.log('Hello, World!') // Code will work, but it's recommended to use semi-colons in JavaScript
+
+/*  
    5. Print a number. (Bonus: Print two numbers added together).
+*/
+console.log(42); // Task 5: Print a number
+console.log(3 + 5); // Bonus: Print two numbers added together
+
+/*  
    6. Print multiple messages one after the other.
+*/
+console.log('Message 1');
+console.log('Message 2');
+console.log('Message 3');
+
+/*  
    7. Print two messages on the same line.
+*/
+console.log('Message 1', 'Message 2');
+
+/*  
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+*/
+console.log('Quoth the Raven "Nevermore"');
+
+/*  
    9. Other. You choose!
 */
+console.log('Hello, ' + 'World!'); // Concatenate strings
